13bd07968b269efb01d9a8a2a50c2907c1201a8b
Dang Nguyen <dang.nguyen@employmenthero.com>
2026-02-24 11:12:16 +0700

    add tsx config

---
+++ packages/hydra.el
@@ -0,0 +1,25 @@
+;;; hydra.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2026 Dang Nguyen
+;;
+;; Author: Dang Nguyen <dangnguyen@EH-MAC-768-dangnguyen>
+;; Maintainer: Dang Nguyen <dangnguyen@EH-MAC-768-dangnguyen>
+;; Created: February 24, 2026
+;; Modified: February 24, 2026
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/dangnguyen/hydra
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'hydra)
+;;; hydra.el ends here
